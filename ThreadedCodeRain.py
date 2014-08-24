e8c981270888028634b24f027ef49821db458cbb
King Mak <kingmak@hotmail.com>
2014-08-24 16:13:48 -0500

    Update ThreadedCodeRain.py

--- ThreadedCodeRain.py
+++ ThreadedCodeRain.py
@@ -3,15 +3,14 @@ import os, time, random, threading
 x = ['0', '1', '2', '3' ,'4', '5', '6', '7']
 y = ['8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 
-names = ['Allah', 'Uzumaki Naruto', 'WarKingMak', 'Neo', 'Minato', 'Jiraiya',
-          'Kakashi', 'Python', '1337', 'Hacker', 'War', 'Peace', 'Samurai',
-          'Luffy', 'Light', 'Rasen Shuriken', 'Bijuu Dama', 'Frog Kumite',
-          'Kunai', 'Sage Mode', 'Rasengan', 'Dench']
+names = ['Allah', 'Uzumaki Naruto', 'WarKingMak', 'Neo', 'Minato', 'Jiraiya', 'Kakashi', 'Python', '1337', 'Hacker', 
+         'War', 'Peace', 'Samurai', 'Luffy', 'Light', 'Rasen Shuriken', 'Bijuu Dama', 'Frog Kumite', 'Kunai', 
+         'Sage Mode', 'Rasengan', 'Dench']
 
 chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-          '!', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', '~', '`', '|', "/", ':', ';', '"', '<', '>', '?',
-          '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+         '!', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', '~', '`', '|', "/", ':', ';', '"', '<', '>', '?',
+         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def ColorChange():
     while True:
