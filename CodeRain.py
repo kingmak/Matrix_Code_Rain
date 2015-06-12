cd7d8c17545656e64ea4729ff6a6de3d744f7e9e
King Mak <kingmak@hotmail.com>
2015-06-12 01:36:18 -0500

    Update CodeRain.py

--- CodeRain.py
+++ CodeRain.py
@@ -1,12 +1,15 @@
 from os import system
 from random import sample
+from platform import platform
 
-system('title Matrix Code Rain by KingMak & color 0a')
+if 'Windows' in platform():
+    system('title Matrix Code Rain by KingMak & color 0a')
+    
 Chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
           '!', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', '~', '`', '|', "/", ':', ';', '"', '<', '>', '?',
           '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 while True:
-    RandomCharList = sample(Chars, 40)
-    print ' '.join(RandomCharList)
+    print ' '.join(sample(Chars, 40))
+
